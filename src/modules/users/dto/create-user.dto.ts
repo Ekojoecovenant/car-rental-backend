73b7f6d5a64d92f8beea4e29b882de07d78b6cc0
Ekojoecovenant <covenantekojoe@gmail.com>
2025-12-19 20:48:30 +0000

    feat(users): add DTOs for user validation

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  fullName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/, {
+    message:
+      'Password must contain uppercase, lowercase, number and special character',
+  })
+  password: string;
+
+  @IsOptional()
+  @IsPhoneNumber('GH', {
+    message: 'Please provide a valid Ghanaian phone number',
+  })
+  phoneNumber?: string;
+}
