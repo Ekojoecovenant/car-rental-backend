855f85390ccda898f7e57c44127e2737cdcabae5
Ekojoecovenant <covenantekojoe@gmail.com>
2025-12-19 20:21:16 +0000

    feat(database): configure Neon PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    // Make ConfigModule available globally
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    // Setup TypeORM with our database config
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getDatabaseConfig,
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
