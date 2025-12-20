b1aeac9304484cda5ceb81f0eff5009fa47900bb
Ekojoecovenant <covenantekojoe@gmail.com>
2025-12-20 06:57:15 +0000

    feat(users): create users controller with REST endpoints

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,72 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.usersService.create(createUserDto);
+
+    // Don't return password in response
+    const { password, ...result } = user;
+    void password;
+    return {
+      message: 'User created successfully',
+      data: result,
+    };
+  }
+
+  @Get()
+  async findAll() {
+    const users = await this.usersService.findAll();
+
+    return {
+      message: 'Users retrieved successfully',
+      data: users,
+    };
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findById(id);
+
+    return {
+      message: 'User retrieved successfully',
+      data: user,
+    };
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const user = await this.usersService.update(id, updateUserDto);
+
+    const { password, ...result } = user;
+    void password;
+
+    return {
+      messagee: 'User updated succesfully',
+      data: result,
+    };
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: string) {
+    await this.usersService.remove(id);
+  }
+}
